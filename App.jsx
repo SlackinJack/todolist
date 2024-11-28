5f9fccfb0f00a2f07368c72853ecd03b9715f33f
SlackinJack <41076523+SlackinJack@users.noreply.github.com>
2024-11-27 21:01:11 -0700

    lab4

--- App.jsx
+++ App.jsx
@@ -5,18 +5,20 @@ import ToDoForm from './modules/ToDoForm.jsx';
 import Styles from './Styles.jsx';
 
 function App() {
-    const [tasks, setTasks] = useState(
-        [
-            'Do laundry',
-            'Go to gym',
-            'Walk dog'
-        ]
-    );
+    const [tasks, setTasks] = useState([
+        'Do laundry',
+        'Go to gym',
+        'Walk dog'
+    ]);
+
+    const addTask = (taskText) => {
+      setTasks([...tasks, taskText]);
+    };
     
     return (
         <SafeAreaView>
             <ToDoList tasks={tasks} />
-            <ToDoForm />
+            <ToDoForm addTask={addTask} />
         </SafeAreaView>
     );
 }
--- modules/ToDoForm.jsx
+++ modules/ToDoForm.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 import Styles from '../Styles.jsx';
 
-function ToDoForm() {
+function ToDoForm({ addTask }) {
+    const [taskText, setTaskText] = React.useState('');
+
     return (
         <View style={Styles.form}>
-            <TextInput style={Styles.input} placeholder="Add a new task..." />
-            <Button title="Add" />
+            <TextInput 
+              style={Styles.input} 
+              placeholder="Add a new task..." 
+              onChangeText={(text) => setTaskText(text)}
+              value={taskText}
+            />
+            <Button title="Add Task" onPress={() => addTask(taskText)} />
         </View>
     );
 }
